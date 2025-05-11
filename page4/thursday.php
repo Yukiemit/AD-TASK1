83eaea2736c6344ff96d17604d4c88972b5d8fb1
yukiemit <202310242@fit.edu.ph>
2025-05-12 01:15:38 +0800

    feat (generator page) added a structure for friday schedule and also added css

--- page4/thursday.php
+++ page4/thursday.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wednesday</title>
+    <title>Thursday</title>
     <link rel="stylesheet" href="/AD-TASK1/page/assets/css/style.css">
 </head>
 <body>
     <div class="container">
-        <h1>Wednesday Schedule</h1>
+        <h1>Thursday Schedule</h1>
         <div class="schedule">
             <?php
-            $day = "Wednesday";
+            $day = "Thursday";
             $schedule = [];
 
-            if ($day == "Wednesday") {
+            if ($day == "Thursday") {
                 $schedule = [
-                "9:00 AM - 10:50 AM | Technopreneur (FTIC)",
-                "1:00 PM - 3:00 PM | Physics Laboratory (F1009)",
+                "7:00 AM - 8:50 AM | Mobile Application Development (F1209)",
+                "10:00 AM - 12:50 PM | Business Process (F1211)",
+                "1:00 PM - 2:50 PM | Purposive Communication (F611)"
                 ];
             }
 
