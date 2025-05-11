13ca6fe3d124d5f991cf6934fdc789a1a071b9a5
yukiemit <202310242@fit.edu.ph>
2025-05-11 23:09:25 +0800

    feat (landing page) created a landing page and added a structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <h1>My Schedule</h1>'
+    <div class="days-container">
+        <?php
+        $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        for ($i = 0; $i < count($days); $i++) {
+            echo '<a class='day-link' href="' . strtolower($days[$i]) . '.php">' . $days[$i] . '</a>';
+        }
+        ?>
+    </div>
+</body>
+</html>
